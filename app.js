7b80f628072f241548e4bf94b0faf640fd0658b2
sanjaymeta <sanjay@metaversitytechnologies.com>
2024-04-10 11:13:17 +0530

    add

---
+++ app.js
@@ -0,0 +1,53 @@
+// sum of array of elements
+
+// let item = [1,2,3,4,4]
+
+// let sum = 0;
+
+// for(let i = 0 ; i<item.length ; i++){
+//     sum +=item[i]
+// }
+
+// console.log(sum);
+
+//sum of array of elements
+
+// let sum = 0 ;
+// for(let i =0 ;i<5; i++){
+//   sum +=i
+// }
+
+// console.log(sum);
+
+
+//2:javascript string reverse 
+
+// let str = "sanjay kumar"
+
+// console.log(str);
+
+// let newstr = '';
+
+// for(var i=str.length-1; i>=0; i--){
+//     newstr +=str[i]
+// }
+
+// console.log(newstr);
+
+
+//3:count of each single letter in the string "sanjay".
+
+//  let str = "sanjay"
+
+//  let newstr = {}
+
+//  for(var i=0; i<str.length ;i++){
+//     let letter = str[i];
+//     if(newstr[letter]){
+//         newstr[letter]++
+//     }else{
+//         newstr[letter]=1
+//     }
+//  }
+//  console.log(newstr);
+
